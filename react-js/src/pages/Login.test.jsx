dd62055e5998f426f4acee39ac0c87c8afae6558
IraEiza <iratze.eizaguirre@gmail.com>
2024-06-10 20:04:15 +0100

    Fixed! :D

--- react-js/src/pages/Login.test.jsx
+++ react-js/src/pages/Login.test.jsx
@@ -1,20 +1,18 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 import { Login } from "./Login";
 
 describe("Login", () => {
   it("redirects to recipe page after login", async () => {
-    const user = userEvent.setup()
+    const user = userEvent.setup();
 
-    render(<Login />)
+    render(<Login />);
 
-    await user.type(screen.getByText('Your email'))
-    await user.type(screen.getByText('Your email'))
-    await user.click(screen.getByRole("button", { name: "Login" }))
-
-    //await page.getByRole("button", { name: "Login" }).press("Enter")
+    await user.type(screen.getByText('Your email'), 'linustorvalds@gmail.com');
+    await user.type(screen.getByText('Your email'), 'ilovecats');
+    await user.click(screen.getByRole("button", { name: "Login" }));
 
     expect(true).toBe(true);
   });
